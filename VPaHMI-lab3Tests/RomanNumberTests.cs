730d6973a40d89b1f1fedf848b9ecb4167332391
surrepoff <alexander.bessonov548@gmail.com>
2022-03-03 22:21:44 +0700

    Added test template

---
+++ VPaHMI-lab3Tests/RomanNumberTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VPaHMI_lab3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPaHMI_lab3.Tests
+{
+    [TestClass()]
+    public class RomanNumberTests
+    {
+        [TestMethod()]
+        public void CloneTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
